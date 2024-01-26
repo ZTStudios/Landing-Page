285a1ee14e06a322ca72fc4d92d9734944a17544
Bryan Turrubiates <61299735+bryantg20zt@users.noreply.github.com>
2024-01-26 19:13:58 +0000

    feat: (App) Custom Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://bryantg20zt.github.io',
-  base: '/Landing-Page'
+  site: 'https://ztstudios.com.mx',
 });
